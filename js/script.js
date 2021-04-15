86e66f40ba9708fd5c945f3eadb4f70f068ba3e3
Alessandro Venti <alessandro.venti@icloud.com>
2021-04-15 16:26:35 +0200

    80% done

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
+function apiEmail() {
+  for (let i = 0; i < 10; i++) {
+    $.ajax({
+      url: "https://flynn.boolean.careers/exercises/api/random/mail",
+      method: 'GET',
+      success: function(data) {
+          let singleEmail = data.response;
+          $('#jslist').append(`<li>${singleEmail}</li>`); 
+          console.log(singleEmail);
+      },
+      error: function() {
+          console.log('error');
+      } 
+    });
+  };
+}
 
+// function view(singleEmail) {
+//   $('#jslist').append(`<li>${singleEmail}</li>`); 
+// }
 
-document.addEventListener('DOMContentLoaded', init);
+$(document).ready(apiEmail);
